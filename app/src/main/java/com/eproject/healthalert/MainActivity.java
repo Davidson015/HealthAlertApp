5f84b892c9467fa8e55c7aa5df80cee5bcebbae2
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-11 11:20:55 +0100

    Feat(Main): added functionality to Register and Login Buttons

--- app/src/main/java/com/eproject/healthalert/MainActivity.java
+++ app/src/main/java/com/eproject/healthalert/MainActivity.java
@@ -3,7 +3,9 @@ package com.eproject.healthalert;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     Integer[] titles={R.string.dd1,R.string.dd2,R.string.dd3,R.string.dd4};
     Integer[] descriptions={R.string.df1,R.string.df2,R.string.df3,R.string.df4};
     Timer timer = new Timer();
+
+    Button register_btn;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +53,22 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         timer.schedule(timerTask, 4000, 4000);
+
+        // Initialize Navigation buttons
+        register_btn = findViewById(R.id.register_btn);
+        login_btn = findViewById(R.id.login_btn);
+
+        // Register button
+        register_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        // Login button
+        login_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
 
     }
 
