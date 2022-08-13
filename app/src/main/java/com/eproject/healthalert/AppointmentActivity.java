1b2839f1dd24503e2677e934cc263148b93a0c79
timmySpark <timmyspark1@gmail.com>
2022-08-13 12:15:06 +0100

    Feat(appointment Activity):updated AppointmentActivity designs , added add button functionality

--- app/src/main/java/com/eproject/healthalert/AppointmentActivity.java
+++ app/src/main/java/com/eproject/healthalert/AppointmentActivity.java
@@ -1,34 +1,62 @@
 package com.eproject.healthalert;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.eproject.healthalert.adapter.AppointmentAdapter;
 import com.eproject.healthalert.model.Appointment;
-import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class AppointmentActivity extends AppCompatActivity {
 
     CardView card;
-    Button addbtn;
     Intent intent;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    private long pressedTime;
 
     private RecyclerView appointment_recyclerView;
-
     private ArrayList<Appointment> appointmentArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+        Button addbtn = findViewById(R.id.add_appointment_btn);
+
+        // Navigation Drawer
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.blue));
+        getSupportActionBar().setTitle("");
+
+        // Initializing drawer layout and actionbarToggle
+        drawer = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // Initializing NavigationView
+        navigationView = findViewById(R.id.nav_view);
+        setupDrawerContent(navigationView);
 
 //        Setting Recycler View for Appointment cards
         appointment_recyclerView = findViewById(R.id.recyclerview);
@@ -50,6 +78,73 @@ public class AppointmentActivity extends AppCompatActivity {
         appointment_recyclerView.setAdapter(appointmentAdapter);
 
         // make card checkable
+        addbtn.setOnClickListener(v -> {
+            Intent intent = new Intent(AppointmentActivity.this, AddAppointmentActivity.class);
+            startActivity(intent);
+        });
 
+    }
+
+    // Creating the setUpDrawerContent method
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    selectDrawerItem(menuItem);
+                    return true;
+                });
+    }
+
+    // Creating the selectDrawerItem method to handle navigation item clicks
+    @SuppressLint("NonConstantResourceId")
+    public void selectDrawerItem(MenuItem menuItem) {
+        Intent intent;
+        switch (menuItem.getItemId()) {
+
+            case R.id.appointments:
+                drawer.closeDrawers();
+                break;
+//            case R.id.settings:
+//                intent = new Intent(ScheduleActivity.this, SettingsActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            case R.id.health_vitals:
+//                intent = new Intent(ScheduleActivity.this, AboutActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            case R.id.medicine:
+//                intent = new Intent(ScheduleActivity.this, AboutActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            case R.id.exit_app:
+//                // Confirming if the user wants to exit the app
+//                confirmExit();
+//                break;
+            default:
+                break;
+        }
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawer.closeDrawers();
+    }
+
+    // Creating the onBackPressed method to handle the back button press
+    @SuppressLint("RtlHardcoded")
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(Gravity.LEFT)) {
+            drawer.closeDrawer(Gravity.LEFT);
+        } else {
+            if (pressedTime + 2000 > System.currentTimeMillis()) {
+                super.onBackPressed();
+                finishAffinity();
+            } else {
+                Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+            }
+            pressedTime = System.currentTimeMillis();
+        }
     }
 }
