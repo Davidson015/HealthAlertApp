6b0aaa776ee27573dc917b6827090f140128fca8
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-14 13:44:22 +0100

    Feat(appointments): worked on recycler view of appointments from the db

--- app/src/main/java/com/eproject/healthalert/AppointmentActivity.java
+++ app/src/main/java/com/eproject/healthalert/AppointmentActivity.java
@@ -1,5 +1,8 @@
 package com.eproject.healthalert;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,6 +15,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -20,6 +24,10 @@ import android.widget.Toast;
 import com.eproject.healthalert.adapter.AppointmentAdapter;
 import com.eproject.healthalert.model.Appointment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -36,10 +44,14 @@ public class AppointmentActivity extends AppCompatActivity {
     Toolbar toolbar;
     NavigationView navigationView;
     private long pressedTime;
+
+    // Creating an instance of firebase db
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://health-alert-52481-default-rtdb.firebaseio.com");
+
     SharedPreferences pref;
 
-    private RecyclerView appointment_recyclerView;
-    private ArrayList<Appointment> appointmentArrayList;
+    private RecyclerView appointmentRecyclerView;
+    private ArrayList<Appointment> appointmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,30 +79,44 @@ public class AppointmentActivity extends AppCompatActivity {
         setupDrawerContent(navigationView);
 
 //        Setting Recycler View for Appointment cards
-        appointment_recyclerView = findViewById(R.id.recyclerview);
+        appointmentRecyclerView = findViewById(R.id.recyclerview);
 
-        appointmentArrayList = new ArrayList<>();
+        // initialing arraylist for appointment
+        appointmentList = new ArrayList<>();
 
-        appointmentArrayList.add(new Appointment("", apptId++, "Dr Adam Smith ,Dentist Meeting", "21st Aug 2022", "10:00 am", "New City Clinic"));
-        appointmentArrayList.add(new Appointment("", apptId++, "Dr Adam Smith ,Dentist Meeting", "21st Aug 2022", "10:00 am", "New City Clinic"));
-        appointmentArrayList.add(new Appointment("", apptId++, "Dr Adam Smith ,Dentist Meeting", "21st Aug 2022", "10:00 am", "New City Clinic"));
-        appointmentArrayList.add(new Appointment("", apptId++, "Dr Angel Smith,Gynecologist", "23rd Sep 2022", "08:00 am", "Lagoon Hospital"));
-        appointmentArrayList.add(new Appointment("", apptId++, "Dr Angel Smith,Gynecologist Checkup", "23rd Sep 2022", "08:00 am", "Lagoon Hospital"));
-        appointmentArrayList.add(new Appointment("", apptId++, "Dr Beau Hightower , Chiropractic Adjustment ", "21st Aug 2022", "10:00 am", "New City Clinic"));
+        // Getting user email from shared preferences
+        pref = getSharedPreferences("user", MODE_PRIVATE);
+        String userEmail = pref.getString("email", "");
 
+        // Getting appointments from firebase db
+        database.getReference("appointments").orderByChild("userEmail").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.getValue() != null) {
+                    // Getting the appointment values
+                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                        Appointment appointment = childSnapshot.getValue(Appointment.class);
+                        appointmentList.add(appointment);
+                    }
+                }
+                // Setting the adapter for the recycler view
+                AppointmentAdapter appointmentAdapter = new AppointmentAdapter(AppointmentActivity.this, appointmentList);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(AppointmentActivity.this, LinearLayoutManager.VERTICAL, false);
+                appointmentRecyclerView.setAdapter(appointmentAdapter);
+                appointmentRecyclerView.setLayoutManager(new LinearLayoutManager(AppointmentActivity.this));
+            }
 
-        AppointmentAdapter appointmentAdapter = new AppointmentAdapter(this, appointmentArrayList);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-
-        appointment_recyclerView.setLayoutManager(linearLayoutManager);
-        appointment_recyclerView.setAdapter(appointmentAdapter);
-
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+            }
+        });
 
         addbtn.setOnClickListener(v -> {
             intent = new Intent(AppointmentActivity.this, AddAppointmentActivity.class);
             startActivity(intent);
         });
-
 
     }
 
