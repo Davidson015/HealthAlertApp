56344c5875d63c5e31d4edc2991b8b5ba9e417c5
timmySpark <timmyspark1@gmail.com>
2022-08-14 21:23:16 +0100

    Feat(health):created health activity, added functionality for drawer layout view

---
+++ app/src/main/java/com/eproject/healthalert/HealthActivity.java
@@ -0,0 +1,146 @@
+package com.eproject.healthalert;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HealthActivity extends AppCompatActivity {
+    Intent intent;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    Button addbtn;
+    NavigationView navigationView;
+    SharedPreferences pref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+
+
+        addbtn = findViewById(R.id.add_vitals_btn);
+
+        addbtn.setOnClickListener(v -> {
+            intent = new Intent(HealthActivity.this, AddHealthVitalsActivity.class);
+            startActivity(intent);
+        });
+
+
+
+        // Navigation Drawer
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.transparent));
+        getSupportActionBar().setTitle("Personal Health Vitals");
+
+        // Initializing drawer layout and actionbarToggle
+        drawer = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // Initializing NavigationView
+        navigationView = findViewById(R.id.nav_view);
+        setupDrawerContent(navigationView);
+
+    }
+
+    // Creating the setUpDrawerContent method
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    selectDrawerItem(menuItem);
+                    return true;
+                });
+    }
+
+    // Creating the selectDrawerItem method to handle navigation item clicks
+    @SuppressLint("NonConstantResourceId")
+    public void selectDrawerItem(MenuItem menuItem) {
+        Intent intent;
+        switch (menuItem.getItemId()) {
+            case R.id.home:
+                finish();
+                break;
+            case R.id.appointments:
+                intent = new Intent(com.eproject.healthalert.HealthActivity.this, com.eproject.healthalert.AppointmentActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.health_vitals:
+                drawer.closeDrawers();
+                break;
+            case R.id.medicine:
+                intent = new Intent(HealthActivity.this, MedicineDosageActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.feedback:
+                intent = new Intent(HealthActivity.this, FeedbackActivity.class);
+                startActivity(intent);
+                break;
+//            case R.id.contact:
+//                intent = new Intent(FeedbackActivity.this, ContactActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            case R.id.help:
+//                intent = new Intent(FeedbackActivity.this, HelpActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            case R.id.settings:
+//                intent = new Intent(FeedbackActivity.this, SettingsActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+            case R.id.logout:
+                // Redirecting User to MainActivity
+                intent = new Intent(com.eproject.healthalert.HealthActivity.this, MainActivity.class);
+
+                // Displaying a Toast message
+                Toast.makeText(com.eproject.healthalert.HealthActivity.this, "See you soon!", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+                finishAffinity();
+
+                // Clearing the SharedPreferences
+                SharedPreferences.Editor editor = pref.edit();
+                editor.clear();
+                editor.apply();
+
+                break;
+            default:
+                break;
+        }
+        drawer.closeDrawers();
+    }
+
+    // Overriding the onBackPressed method
+    @SuppressLint("RtlHardcoded")
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(Gravity.LEFT)) {
+            drawer.closeDrawer(Gravity.LEFT);
+        } else {
+            super.onBackPressed();
+            finish();
+        }
+    }
+
+
+}
